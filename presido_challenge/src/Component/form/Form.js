61bbd3ab03701171e6129547b059085468e88e4c
Shreyajain30 <109917905+Shreyajain30@users.noreply.github.com>
2024-05-26 16:42:08 +0530

    new commit

--- presido_challenge/src/Component/form/Form.js
+++ presido_challenge/src/Component/form/Form.js
@@ -1,105 +1,18 @@
-
 import { useState } from "react";
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './Form.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Form.css";
 function Form({ onFormSubmit }) {
   const [inputValue, setInputValue] = useState({ email: "", password: "" });
-  const [error, setError] = useState({ email: false, password: false });
+  const [user, setUser] = useState({ buyer: false, seller: false });
+
   const [logIn, setLogIn] = useState({
     title: false,
     user: false,
     button: "SIGNUP",
   });
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const a = /[a-z]/.test(inputValue.email);
-    const b = /[A-Z]/.test(inputValue.password);
-    const c = /[!@#$%^&*()_+-=[]{};’:”\|,.<>\/?]+/.test(inputValue.password);
-
-    const e = inputValue.email.indexOf("@");
-    const f = inputValue.email.indexOf(".");
-
-    const emailInput = document.getElementById("email");
-    const passwordInput = document.getElementById("password");
-    console.log(inputValue);
-    // console.log(e,f,passwordError,passwordInput);
-    if (inputValue.email === null || e === -1 || f === -1) {
-      setError({ email: true, password: error.password });
-      emailInput.style.border = "1px solid red";
-      setTimeout(() => {
-        setError({ email: false, password: error.password });
-        emailInput.style.border = "1px solid grey";
-      }, 3000);
-    } else if (!a || !b || c === -1 || inputValue.password.length < 8) {
-      setError({ email: error.email, password: true });
-      passwordInput.style.border = "1px solid red";
-      setTimeout(() => {
-        setError({ email: error.email, password: false });
-        passwordInput.style.border = "1px solid grey";
-      }, 3000);
-    } else {
-      console.log(inputValue.email);
-      let myHeaders, raw, requestOptions;
-      if (logIn.button === "SIGNUP") {
-        myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("Authorization", "Bearer {{authToken}}");
-
-        raw = JSON.stringify({
-          email: inputValue.email,
-          password: inputValue.password
-        });
-
-        requestOptions = {
-          method: "POST",
-          headers: myHeaders,
-          body: raw,
-          redirect: "follow",
-        };
-
-        fetch("https://xerver.onrender.com/api/users/sign-up", requestOptions)
-          .then((response) => response.text())
-          .then((result) => console.log(result))
-          .catch((error) => console.log("error", error));
-      } else {
-        myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("Authorization", "Bearer {{authToken}}");
-
-        raw = JSON.stringify({
-          email: inputValue.email,
-          password: inputValue.password
-        });
-
-        requestOptions = {
-          method: "POST",
-          headers: myHeaders,
-          body: raw,
-          redirect: "follow",
-        };
-
-        fetch("https://xerver.onrender.com/api/users/login", requestOptions)
-          .then((response) => response.text())
-          .then((result) => console.log(result))
-          .catch((error) => console.log("error", error));
-      }
-
-      setLogIn({ title: logIn.title, user: logIn.user, button: "loading..." });
-      setTimeout(() => {
-        setLogIn({
-          title: logIn.title,
-          user: logIn.user,
-          button: logIn.title ? "LOGIN" : "SIGNUP",
-        });
-        setInputValue({ email: "", password: "" });
-      }, 2000);
-
-      // alert("Accepted!");
-    }
-  };
+  const handleSubmit = (event) => {};
   return (
     <div className="container">
       <div className="form">
@@ -110,10 +23,63 @@ function Form({ onFormSubmit }) {
             <h1 className="main">{logIn.title ? "LOG IN" : "SIGN UP"}</h1>
           </div>
           <hr />
-          <div className="email box">
-            <label htmlFor="email">
-              First Name
-            </label>
+          <div className="firstName box">
+            <label>First Name</label>
+            <input
+              type="text"
+              placeholder="Enter First Name"
+              value={inputValue.firstName}
+              onChange={(e) =>
+                setInputValue({
+                  firstName: e.target.value,
+                  lastName: inputValue.lastName,
+                  phone: inputValue.phone,
+                  email: inputValue.email,
+                  password: inputValue.password,
+                })
+              }
+              required
+            />
+          </div>
+          <div className="name box">
+            <label>Last Name</label>
+            <input
+              type="text"
+              placeholder="Enter Last Name"
+              value={inputValue.lastName}
+              onChange={(e) =>
+                setInputValue({
+                  firstName: inputValue.firstName,
+                  lastName: e.target.value,
+                  phone: inputValue.phone,
+                  email: inputValue.email,
+                  password: inputValue.password,
+                })
+              }
+              required
+            />
+          </div>
+          <div className="name box">
+            <label>Phone Number</label>
+            <input
+              type="text"
+              placeholder="Enter Last Name"
+              value={inputValue.phone}
+              onChange={(e) =>
+                setInputValue({
+                  firstName: inputValue.firstName,
+                  lastName: inputValue.lastName,
+                  phone: e.target.value,
+                  email: inputValue.email,
+                  password: inputValue.password,
+                })
+              }
+              required
+            />
+          </div>
+          
+          <div className=" box">
+            <label htmlFor="email">Email</label>
             <input
               type="text"
               id="email"
@@ -121,42 +87,18 @@ function Form({ onFormSubmit }) {
               value={inputValue.email}
               onChange={(e) =>
                 setInputValue({
+                  firstName: inputValue.firstName,
+                  lastName: inputValue.lastName,
+                  phone: inputValue.phone,
                   email: e.target.value,
                   password: inputValue.password,
                 })
               }
               required
             />
-            {error.email ? (
-              <p id="invalid_email">Please enter valid email Id!</p>
-            ) : null}
           </div>
-          <div className="email box">
-            <label htmlFor="email">
-              Email
-            </label>
-            <input
-              type="text"
-              id="email"
-              placeholder="admin@gmail.com"
-              value={inputValue.email}
-              onChange={(e) =>
-                setInputValue({
-                  email: e.target.value,
-                  password: inputValue.password,
-                })
-              }
-              required
-            />
-            {error.email ? (
-              <p id="invalid_email">Please enter valid email Id!</p>
-            ) : null}
-          </div>
-
-          <div className="password box">
-            <label htmlFor="password">
-              Phone Number
-            </label>
+          <div className=" box">
+            <label htmlFor="password">Password</label>
             <input
               type="password"
               id="password"
@@ -164,6 +106,9 @@ function Form({ onFormSubmit }) {
               value={inputValue.password}
               onChange={(e) =>
                 setInputValue({
+                  firstName: inputValue.firstName,
+                  lastName: inputValue.lastName,
+                  phone: inputValue.phone,
                   email: inputValue.email,
                   password: e.target.value,
                 })
@@ -171,29 +116,34 @@ function Form({ onFormSubmit }) {
               required
             />
           </div>
-
-          <div className="box forget">
-            <div>
-              {error.password ? (
-                <p id="invalid_password">
-                  Must be 8 or more characters and contain at least one
-                  uppercase, one lowercase and special character!
-                </p>
-              ) : null}
-            </div>
-            <div className="forgetId">
-              <a href="#" id="forget">
-                Forget Password?
-              </a>
-            </div>
+          <div className=" box">
+            <label>Choose Any one</label>
+            <label>
+              <input
+                type="radio"
+                value="option1"
+                checked={user.buyer}
+                onChange={(e) => setUser({ buyer: true, seller: false })}
+              />
+              Buyer
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="option2"
+                checked={user.seller}
+                onChange={(e) => setUser({ buyer: false, seller: true })}
+              />
+              Seller
+            </label>
           </div>
 
           <div className="box">
             <button className="button_container">
               <p className="button_content" id="submit">
-                {logIn.button}
+                {logIn.button} 
               </p>
-              <i id="button_icon" className="fa-solid fa-user-plus"></i>
+              
             </button>
           </div>
           <div className="user box">
